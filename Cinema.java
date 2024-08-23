7b1afa0c86ea59a4c5f92b0f90ad5bcaaf565e07
Aneeq Dilimatrix <aneeq.ahmadh@dilimatrix.com>
2024-08-23 23:56:01 +0530

    stage 3- implemented booking seat and ticket price according to seat

--- Cinema.java
+++ Cinema.java
@@ -3,37 +3,113 @@ package cinemaBooking;
 import java.util.Scanner;
 
 public class Cinema {
+    private Scanner sc;
+    int row, column;
+    char[][] seats;
+
+    public Cinema() {
+        sc = new Scanner(System.in);
+    }
+
     public static void main(String[] args) {
-        int row, column;
+        Cinema cinema = new Cinema();
 
-        Scanner sc = new Scanner(System.in);
+        cinema.seatingArrangement();
+        cinema.bookSeat();
+    }
 
-        System.out.println("Enter the number of rows");
-        row = sc.nextInt();
+    void userInput() {
+        do {
+            System.out.println("Enter the number of rows");
+            row = sc.nextInt();
+            if (row > 9)
+                System.out.println("MAXIMUM NUMBER OF ROWS CAN BE 9 OR LESS");
+        } while (row > 9);
 
         System.out.println("Enter the number of columns");
         column = sc.nextInt();
+    }
 
+    void printHeader() {
+        System.out.println("Cinema:");
+        System.out.print("  ");
 
-        if (column * row > 60) {
-            int income = (column * (row / 2) * 10) + (column * (row - (row / 2)) * 8);
-            System.out.println("Total income: " + income);
-        } else {
-            System.out.println("Total income: " + column * row * 10);
-        }
-
-/*        System.out.println("Cinema:");
-        System.out.print(" ");
-
-
-        for (int i = 1; i <= row; i++) {
-            System.out.print(" " + i);
+        for (int a = 1; a <= column; a++) {
+            System.out.printf("%d ", a);
         }
         System.out.println();
+    }
 
-        for (int i = 1; i < column; i++) {
+    void printSeats() {
+        printHeader();
 
-            System.out.printf("%d S S S S S S S S\n", i);
-        }*/
+        int x = 1;
+
+        for (char[] seat : seats) {
+            System.out.print(x + " ");
+            for (char c : seat) {
+                System.out.print(c + " ");
+            }
+            x++;
+            System.out.println();
+        }
+    }
+
+    void seatingArrangement() {
+        userInput();
+
+        seats = new char[row][column];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                seats[i][j] = 'S';
+            }
+        }
+
+        printSeats();
+    }
+
+    void bookSeat() {
+        System.out.println("\n====Seat Booking====");
+
+        int r, s;
+
+        while (true) {
+            System.out.println("Enter a row number");
+            r = sc.nextInt();
+
+            if (r <= 0 || r > row) {
+                System.out.println("WRONG INPUT: Entered row number is not matching. Maximum row count is " + row);
+            } else {
+                break;
+            }
+        }
+
+        while (true) {
+            System.out.println("Enter a seat number in that row");
+            s = sc.nextInt();
+            if (s <= 0 || s > column) {
+                System.out.println("WRONG INPUT: Entered column number is not matching. Maximum column count is " + column);
+            } else {
+                break;
+            }
+        }
+
+        seats[r - 1][s - 1] = 'B';
+
+        printSeats();
+
+        /**
+         * ticket prices
+         */
+        if (row * column < 60 ) {
+            System.out.println("Ticket Price: " + 10);
+        } else {
+            if(r<row/2){
+                System.out.println("Ticket Price: " +10);
+            } else {
+                System.out.println("Ticket Price: " + 8);
+            }
+        }
     }
 }
